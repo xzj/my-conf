e0117d1a0897f8992972e50187ee0f96af5cce50
xzj <xzj007@gmail.com>
2017-06-23 14:59:29 +0800

    Added .lein/profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,8 @@
+{:user {:plugins [[lein-localrepo "0.5.3"]
+		  [lein-pprint "1.1.2"]
+                  [lein-cloverage "1.0.9"]
+                  [lein-kibit "0.1.3"]
+                  [lein-bikeshed "0.4.1"]
+                  [lein-ancient "0.6.10"]
+                  [lein-midje "3.2.1"]
+                  ]}}
